26863851af1cb21f9cd1a0e67c6c3443778ddf46
NHTAirXonix <toan02041999@gmail.com>
2022-04-06 00:28:18 +0700

    update ss6

---
+++ src/_06_Inheritance/exercise/circle_and_cylinder/Cylinder.java
@@ -0,0 +1,52 @@
+package _06_Inheritance.exercise.circle_and_cylinder;
+
+public class Cylinder extends Circle {
+    private double height;
+
+    public Cylinder() {
+    }
+
+    public Cylinder(double height) {
+        this.height = height;
+    }
+
+    public Cylinder(double radius, String color, double height) {
+        super(radius, color);
+        this.height = height;
+    }
+
+    public double getRadius() {
+        return super.getRadius();
+    }
+
+    public void setRadius(double radius) {
+        super.setRadius(radius);
+    }
+
+    public String getColor(String color) {
+        return super.getColor();
+    }
+
+    public void setColor(String color) {
+        super.setColor(color);
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public void setHeight(double height) {
+        this.height = height;
+    }
+
+    public double getVolume() {
+        return getArea() * this.height;
+    }
+
+    @Override
+    public String toString() {
+        return "Cylinder{" + "radius=" + getRadius() + ", color=" + getColor() +
+                ", height=" + height +
+                '}';
+    }
+}
