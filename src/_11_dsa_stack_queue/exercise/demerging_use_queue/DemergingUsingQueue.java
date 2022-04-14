583a14a5cb81aa1345b21cb22f251ae695e69773
NHTAirXonix <toan02041999@gmail.com>
2022-04-14 20:06:31 +0700

    update ss11

--- src/_11_dsa_stack_queue/exercise/demerging_use_queue/DemergingUsingQueue.java
+++ src/_11_dsa_stack_queue/exercise/demerging_use_queue/DemergingUsingQueue.java
@@ -1,15 +1,52 @@
 package _11_dsa_stack_queue.exercise.demerging_use_queue;
 
-import java.util.Stack;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.*;
 
 public class DemergingUsingQueue {
-    public static void main(String[] args) {
-        Stack<String> NU = new Stack<>();
-        Stack<String> NAM = new Stack<>();
-        
+    public static void main(String[] args) throws ParseException {
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+        Queue<Personnel> NAM = new LinkedList<>();
+        Queue<Personnel> NU = new LinkedList<>();
+        Date date1 = sdf.parse("1995-02-23");
+        Date date2 = sdf.parse("2001-10-31");
+        Date date3 = sdf.parse("1999-02-23");
+        Date date4 = sdf.parse("2002-10-31");
+        Date date5 = sdf.parse("1998-02-30");
+        Personnel [] arrayPer = new Personnel[5];
+        arrayPer[0] = new Personnel("Chien", true, date1);
+        arrayPer[1] = new Personnel("Ha", false, date2);
+        arrayPer[2] = new Personnel("Toan", true,date3);
+        arrayPer[3] = new Personnel("Thuy", false,date4);
+        arrayPer[4] = new Personnel("Ngoc", false,date5);
 
 
+        System.out.println("\n---------------------------LIST BEFORE SORT----------------------------");
+        for (Personnel item: arrayPer) {
+            System.out.println(item.toString());
+        }
+        Arrays.sort(arrayPer);
 
+        for (Personnel item : arrayPer) {
+            if (item.isGender()){
+                NAM.add(item);
+            } else {
+                NU.add(item);
+            }
+        }
 
+        List<Personnel> orderedList = new ArrayList<>();
+        while (NU.size() > 0){
+            orderedList.add(NU.remove());
+        }
+        while (NAM.size() > 0){
+            orderedList.add(NAM.remove());
+        }
+        System.out.println("\n----------------------------LIST AFTER SORT----------------------------");
+
+        for (Personnel item: orderedList) {
+            System.out.println(item.toString());
+        }
     }
 }
---
+++ src/_11_dsa_stack_queue/exercise/demerging_use_queue/Personnel.java
@@ -0,0 +1,57 @@
+package _11_dsa_stack_queue.exercise.demerging_use_queue;
+
+import java.util.Date;
+
+public class Personnel implements Comparable<Personnel> {
+    private String name;
+    private boolean gender = true;
+    private int age;
+    private Date date;
+
+    public Personnel() {
+    }
+
+    public Personnel(String name, boolean gender, Date date) {
+        this.name = name;
+        this.gender = gender;
+        this.date = date;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public boolean isGender() {
+        return gender;
+    }
+
+    public void setGender(boolean gender) {
+        this.gender = gender;
+    }
+
+    @Override
+    public String toString() {
+        return "Personnel{" +
+                "name='" + name + '\'' +
+                ", gender=" + gender +
+                ", date=" + date +
+                '}';
+    }
+
+    @Override
+    public int compareTo(Personnel o) {
+        return this.getDate().compareTo(o.getDate());
+    }
+}
